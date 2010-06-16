e0318610a5c289e9feed7d126c314a799d5e9c3e
r0mk1n.work <r0mk1n.work@d2891916-381b-0e49-cd40-51657d3e22bd>
2010-06-16 10:48:19 +0000

    * added db structure * added override app_controller and app_model

---
+++ app/app_controller.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Overrided appController
+ * @author r0mk1n
+ *
+ */
+
+App::import( 'Core', 'Sanitize' );
+
+class AppController extends Controller {
+    var $helpers 		= array ( 'Html', 'Javascript', 'Ajax', 'Form', 'Text', 'Session');
+    var $components     = array( 'Session', 'RequestHandler', 'Cookie' );
+
+    var $user_info      = null;
+    var $user_id        = null;
+    var $user_role      = 'guest';
+
+    var $auth_url       = '/users/login';
+
+    function beforeFilter() {
+        if ( $this->Session->check( "User" ) ) {
+            $this->user_info = $this->Session->read( "User" );
+            $this->user_role = $this->user_info['role'];
+            $this->user_id = $this->user_info['id'];
+        } else {
+            $this->user_info   = null;
+            $this->user_id     = null;
+            $this->user_role   = 'guest';
+        }
+        $this->set( 'User', $this->user_info );
+        if ( !$this->checkAccess( $this->action, $this->access ) ) {
+        	$this->Session->write( 'before_login_url', $this->here );
+        	$this->redirect( $this->auth_url );
+        }
+
+    }
+
+/**
+ * Check access function (ACL - like)
+ * based on $controller->access = array( 'view'=>array( 'user', 'admin' ) )
+ *
+ * @param String $action
+ * @param Array $access
+ * @return Boolean
+ */
+    function checkAccess( $action, $access = '' ) {
+        if ( is_array( $access ) && array_key_exists( $action, $access ) ) {
+            if ( array_key_exists( 'role', $access[$action] ) ) {
+                // access based on role
+                if ( in_array( $this->user_role, $access[$action]['role'])) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if ( in_array( $this->user_role, $access[$action] ) ) {
+                     return true;
+                } else {
+                     return false;
+                }
+            }
+        }
+        // default - no access control,
+        return  true;
+    }
+
+
+}
---
+++ app/config/schema/structure.sql
@@ -0,0 +1,114 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.21 
+MySQL - 5.0.67-community-nt : Database - tricks_cake
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tricks_cake` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `tricks_cake`;
+
+/*Table structure for table `locations` */
+
+DROP TABLE IF EXISTS `locations`;
+
+CREATE TABLE `locations` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `user_id` int(10) unsigned default NULL,
+  `title` varchar(255) default '',
+  `city` varchar(50) default '',
+  `state` varchar(2) default '',
+  `zip` int(10) unsigned default NULL,
+  `address1` varchar(255) default '',
+  `address2` varchar(255) default '',
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `locations` */
+
+/*Table structure for table `tags` */
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `tag` varchar(50) default '',
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tags` */
+
+/*Table structure for table `tags_to_tasks` */
+
+DROP TABLE IF EXISTS `tags_to_tasks`;
+
+CREATE TABLE `tags_to_tasks` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `task_id` int(10) unsigned default NULL,
+  `user_id` int(10) unsigned default NULL,
+  `tag_id` int(10) unsigned default NULL,
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tags_to_tasks` */
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `user_id` int(10) unsigned default NULL,
+  `location_id` int(10) unsigned default NULL,
+  `exp_date` datetime default NULL,
+  `title` varchar(255) default '',
+  `description` text,
+  `url` varchar(255) default '',
+  `complete` enum('yes','no') default NULL,
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tasks` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `email` varchar(50) default '',
+  `pass` varchar(50) default '',
+  `enabled` enum('yes','no') default 'yes',
+  `acivated` enum('yes','no') default 'no',
+  `ac_code` varchar(32) default '',
+  `city` varchar(50) default '',
+  `state` varchar(2) default '',
+  `zip` int(10) unsigned default NULL,
+  `address1` varchar(255) default '',
+  `address2` varchar(255) default '',
+  `role` enum('admin','user') default 'user',
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
